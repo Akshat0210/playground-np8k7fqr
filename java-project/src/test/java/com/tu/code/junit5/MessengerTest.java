890f71b7e08eafc4638133a0d6f258f967a90188
Akshat Sharma <amsharm@transunion.com>
2018-06-20 16:35:17 -0500

    Mockito test case

--- java-project/src/test/java/com/tu/code/junit5/MessengerTest.java
+++ java-project/src/test/java/com/tu/code/junit5/MessengerTest.java
@@ -1,31 +1,33 @@
+// { autofold
 package com.tu.code.junit5;
-
-import static org.mockito.Mockito.mock;
 import static org.mockito.Mockito.verify;
 import static org.mockito.Mockito.when;
-
-import org.junit.jupiter.api.BeforeAll;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
-
-class MessengerBoilerplateTest {
+import org.mockito.InjectMocks;
+import org.mockito.Mock;
+import org.mockito.MockitoAnnotations;
+// }
+class MessengerTest {
 
 	private static final String CLIENT_EMAIL = "someone@somewhere.com";
 	private static final String MSG_CONTENT = "Dear SomeOne Welcome";
 
+	@Mock
 	private Template template;
+	@Mock
 	private Client client;
+	@Mock
 	private MailServer mailServer;
-	private Messenger messenger;
+	@Mock
 	private TemplateEngine templateEngine;
+
+	@InjectMocks
+	private Messenger messenger = new Messenger();
 
 	@BeforeEach
 	public void setUp() {
-		template = mock(Template.class);
-		client = mock(Client.class);
-		mailServer = mock(MailServer.class);
-		templateEngine = mock(TemplateEngine.class);
-		messenger = new Messenger(mailServer, templateEngine);
+		MockitoAnnotations.initMocks(this);
 	}
 
 	@Test
@@ -33,6 +35,7 @@ class MessengerBoilerplateTest {
 		when(client.getEmail()).thenReturn(CLIENT_EMAIL);
 		when(templateEngine.prepareMessage(template, client)).thenReturn(MSG_CONTENT);
 		messenger.sendMessage(client, template);
-		verify(mailServer).send(CLIENT_EMAIL, MSG_CONTENT);
+		verify(mailServer).send("a@A.com", MSG_CONTENT);
 	}
+
 }
