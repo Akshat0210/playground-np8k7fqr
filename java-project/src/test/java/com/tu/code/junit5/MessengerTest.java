34daaf23b61a6182f6ec283faa202efeb5ac8874
Akshat0210 <aakshat.sharma@gmail.com>
2018-06-22 12:34:10 -0500

    remove content

--- java-project/src/test/java/com/tu/code/junit5/MessengerTest.java
+++ java-project/src/test/java/com/tu/code/junit5/MessengerTest.java
@@ -10,32 +10,5 @@ import org.mockito.MockitoAnnotations;
 // }
 class MessengerTest {
 
-	private static final String CLIENT_EMAIL = "someone@somewhere.com";
-	private static final String MSG_CONTENT = "Dear SomeOne Welcome";
-
-	@Mock
-	private Template template;
-	@Mock
-	private Client client;
-	@Mock
-	private MailServer mailServer;
-	@Mock
-	private TemplateEngine templateEngine;
-
-	@InjectMocks
-	private Messenger messenger = new Messenger();
-
-	@BeforeEach
-	public void setUp() {
-		MockitoAnnotations.initMocks(this);
-	}
-
-	@Test
-	public void shouldSendEmail() {
-		when(client.getEmail()).thenReturn(CLIENT_EMAIL);
-		when(templateEngine.prepareMessage(template, client)).thenReturn(MSG_CONTENT);
-		messenger.sendMessage(client, template);
-		verify(mailServer).send("a@A.com", MSG_CONTENT);
-	}
-
+	
 }
