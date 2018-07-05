907562d4ba2027045c876e8acc72d89fdd04c04e
Akshat0210 <aakshat.sharma@gmail.com>
2018-07-05 11:03:29 -0500

    add try catch

--- java-project/src/test/java/com/tu/code/junit5/AssertionsInJUnitFiveTest.java
+++ java-project/src/test/java/com/tu/code/junit5/AssertionsInJUnitFiveTest.java
@@ -27,7 +27,11 @@ class AssertionsInJUnitFiveTest {
 		msg("👍" ," Awesome");
 		System.out.println(String.format("TECHIO> success %s", success));
 		System.out.println("TECHIO> terminal");
-		Thread.sleep(300000);
+		try {
+			Thread.sleep(300000);
+		} catch (InterruptedException e) {
+			e.printStackTrace();
+		}
 		
 	}
 	private static void msg(String channel, String msg) {
